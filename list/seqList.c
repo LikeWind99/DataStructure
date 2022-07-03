2787c4485e468017151261a973d0872ea1cef262
LikeWind99 <53908429+LikeWind99@users.noreply.github.com>
2022-07-04 00:01:49 +0800

    Update seqList.c

--- list/seqList.c
+++ list/seqList.c
@@ -9,11 +9,25 @@ typedef struct SeqList {
 	int length;
 } SeqList;
 
+/** InitSeqList(SeqList &L) -> int
+ * 对顺序表进行初始化
+ * @params:
+ * @param L: 需要进行初始化的顺序表
+ * @return: 如果正确初始化则，返回1
+ */
 int InitSeqList(SeqList &L) {
 	L.length = 0;
 	return OK;
 }
 
+/** InsertSeqList(SeqList &L, int i, ElementType x) -> int
+ * 在顺序表的特定位置插入元素
+ * @params:
+ * @param L: 需要进行插入操作的顺序表
+ * @param i: 插入的位序，从1开始计数
+ * @param data: 插入的元素
+ * @return: 如果插入正确则返回1，否则返回0
+ */
 int InsertSeqList(SeqList &L, int i, ElementType data) {
 	if(L.length >= MAXSIZE) {
 		printf("The sequence list is full.\n");
@@ -31,6 +45,15 @@ int InsertSeqList(SeqList &L, int i, ElementType data) {
 	return OK;
 }
 
+/** ModifySeqList(SeqList &L, int i, ElementType data, ElementType &value) -> int
+ * 在顺序表的特定位置插入元素
+ * @params:
+ * @param L: 需要进行插入操作的顺序表
+ * @param i: 需要修改的位序，从1开始计数
+ * @param data: 现在要改成的元素
+ * @param value: 保存之前的元素
+ * @return: 如果修改正确则返回1，否则返回0
+ */
 int ModifySeqList(SeqList &L, int i, ElementType data, ElementType &value) {
 	if(i < 1 || i > L.length) {
 		printf("Invalid modification location.\n");
@@ -42,6 +65,13 @@ int ModifySeqList(SeqList &L, int i, ElementType data, ElementType &value) {
 	return OK;
 }
 
+/** DeleteSeqList(SeqList &L, int i) -> int
+ * 删除顺序表中指定位序的元素
+ * @params:
+ * @param L: 需要进行删除操作的顺序表
+ * @param i: 需要删除的位序，从1开始计数
+ * @return: 如果删除成功则返回1，否则返回0
+ */
 int DeleteSeqList(SeqList &L, int i) {
 	if(i < 1 || i > L.length) {
 		printf("Invalid delete location.\n");
@@ -54,6 +84,13 @@ int DeleteSeqList(SeqList &L, int i) {
 	return OK;
 }
 
+/** LocateSeqList(SeqList &L, ElementType x) -> int
+ * 查找顺序表中是否存在传入的元素
+ * @params:
+ * @param L: 需要进行查找操作的顺序表
+ * @param x: 需要查询的元素
+ * @return: 如果查询到给定元素则返回位序，否则返回0
+ */
 int LocateSeqList(SeqList &L, ElementType x) {
 	int i = 0;
 	while(i < L.length && L.elem[i] != x) {
@@ -66,6 +103,12 @@ int LocateSeqList(SeqList &L, ElementType x) {
 	return (i+1);
 }
 
+/** DisplaySeqList(SeqList L) -> none
+ * 打印顺序表
+ * @params:
+ * @param L: 需要打印的顺序表
+ * @return: none
+ */
 void DisplaySeqList(SeqList L) {
 	for(int i = 0; i < L.length; i++) {
 		printf("%d ", L.elem[i]);
@@ -101,7 +144,7 @@ int main() {
 	printf("\n");
 	printf("Test modify function: ");
 	printf("Try to modify 8th element\n");
-    ElementType data;
+        ElementType data;
 	ModifySeqList(seqList, 8, 521, data);
 	printf("The original data of 8th is: %d\n", data);
 	DisplaySeqList(seqList);
